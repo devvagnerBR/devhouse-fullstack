f5017008099bfed6e0f0ecc19662d84d89d1c694
devvagner <wagnerluizsg@outlook.com.br>
2023-05-11 13:41:41 -0300

    fix 0.8

--- src/hooks/useRoom.js
+++ src/hooks/useRoom.js
@@ -15,20 +15,21 @@ const useRoom = ( roomId ) => {
 
         const getRoom = async () => {
             const roomRef = ref( db,`rooms/${roomId}` );
-            onValue( roomRef,( res ) => {
-                const data = res.val();
+            onValue( roomRef,( snapshot ) => {
+                const data = snapshot.val();
                 const parsedQuestions = Object.entries( data?.questions ?? {} ).map( ( [key,value] ) => {
+                    
+                    if ( data )
+                        return {
 
-                    return {
-
-                        id: key,
-                        content: value?.content,
-                        author: value?.author,
-                        isHighLighted: value?.isHighlighted,
-                        isAnswered: value?.isAnswered,
-                        likeCount: Object.values( value?.likes ?? {} ).length,
-                        likeId: Object.entries( value?.likes ?? {} ).find( ( [key,like] ) => like?.authorId === user?.id )?.[0]
-                    }
+                            id: key,
+                            content: value?.content,
+                            author: value?.author,
+                            isHighLighted: value?.isHighlighted,
+                            isAnswered: value?.isAnswered,
+                            likeCount: Object.values( value?.likes ?? {} ).length,
+                            likeId: Object.entries( value?.likes ?? {} ).find( ( [key,like] ) => like?.authorId === user?.id )?.[0]
+                        }
                 } )
 
 
