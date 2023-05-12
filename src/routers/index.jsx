3d21411bcb0f55bbcf7036372bc25b87b5b534c3
devvagner <wagnerluizsg@outlook.com.br>
2023-05-11 23:05:47 -0300

    fix 1.3

--- src/routers/index.jsx
+++ src/routers/index.jsx
@@ -13,14 +13,13 @@ const RouterConfig = () => {
 
     <BrowserRouter>
       <AuthContextProvider>
-        <switch>
           <Routes>
             <Route path='/' exact element={<Homepage />} />
             <Route path='/salas/nova' element={<NewRoom />} />
             <Route path='/admin/sala/:id' element={<AdminRoom />} />
             <Route path='/sala/e6abc4b1-4760-452e-a021-1dd7dd1a03e4' element={<Room />} />
           </Routes>
-        </switch>
+
       </AuthContextProvider>
     </BrowserRouter>
   )
